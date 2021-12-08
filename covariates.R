221ad4dc2293e3cbde9bf141b47f422d82151dbc
Brown <norah.brown2@gmail.com>
2021-12-08 14:47:09 -0800

    Fixed the problem with gather and filter

--- covariates.R
+++ covariates.R
@@ -46,17 +46,14 @@ Atnarko_sample_age_pdo<-left_join(Atnarko_sample_age_pdo, pdo_brood_sync,  by=c(
 Atnarko_sample_age_pdo<-left_join(Atnarko_sample_age_pdo, pdo_brood_lag1,  by=c("Brood_Year_Lag_1" = "Year"))
 Atnarko_sample_age_pdo<-left_join(Atnarko_sample_age_pdo, pdo_brood_lag2,  by=c("Brood_Year_Lag_2" = "Year"))
 
-Atnarko_sample_age_pdo_gathered<-Atnarko_sample_age_pdo %>% gather(key="PDO_match", value="PDO.summer.av", PDO_run_sync,  PDO_run_lag1,PDO_run_lag2,  PDO_brood_sync, PDO_brood_lag1, PDO_brood_lag2) %>% 
+Atnarko_sample_age_pdo_gathered<-Atnarko_sample_age_pdo %>% pivot_longer(names_to="PDO_match", values_to="PDO.summer.av", cols=c(PDO_run_sync,  PDO_run_lag1,PDO_run_lag2,  PDO_brood_sync, PDO_brood_lag1, PDO_brood_lag2)) %>% 
                                                    mutate(Temperature_pattern = ifelse(PDO.summer.av > 0, "Hot years", "Cold Years"))
   
-
-Atnarko_sample_age_pdo_gathered$PDO_match<-as.factor(Atnarko_sample_age_pdo_gathered$PDO_match)
-levels(Atnarko_sample_age_pdo_gathered$PDO_match)
-
 
 # Plotting ----------------------------------------------------------------
 
 # Plot pdo vs. Average escapement
+Atnarko_sample_age_pdo_gathered$PDO_match<-as.factor(Atnarko_sample_age_pdo_gathered$PDO_match)
 levels(Atnarko_sample_age_pdo_gathered$PDO_match)
 pdo_matches = unique(Atnarko_sample_age_pdo_gathered$PDO_match)
 pdo_plots = list()
@@ -80,7 +77,7 @@ ggsave(allpdo_plot, file="Plots/PDO.tiff")
 
 # Do again with just 3 and 6 year old fish to see if zoomed in the pattern is there
 for(pdo_ in pdo_matches) {
-  pdo_plots_age3_6[[pdo_]] = ggplot(Atnarko_sample_age_pdo_gathered%>% filter(PDO_match == pdo_, Age_Class==c(3,6)), aes( x=PDO.summer.av, y=Average_Escapement, col=as.factor(Age_Class), fill=as.factor(Age_Class))) + 
+  pdo_plots_age3_6[[pdo_]] = ggplot(Atnarko_sample_age_pdo_gathered%>% filter(PDO_match == pdo_, Age_Class %in% c(3,6)), aes( x=PDO.summer.av, y=Average_Escapement, col=as.factor(Age_Class), fill=as.factor(Age_Class))) + 
     geom_point(aes(shape=Temperature_pattern)) + geom_smooth()+
     ggtitle(Atnarko_sample_age_pdo_gathered$PDO_match[Atnarko_sample_age_pdo_gathered$PDO_match== pdo_])
 }
@@ -92,7 +89,7 @@ ggsave(allpdo_plot_3_6, file="Plots/PDO_3_6.tiff")
 
 # Correlation plotting, don't use the gathered file use the spread file 
 Atnarko_sample_pdo_correlation<-Atnarko_sample_age_pdo %>% select(Average_Escapement, PDO_run_sync, PDO_run_lag1, PDO_run_lag2, 
-                                                                  PDO_brood_sync, PDO_brood_lag1, PDO_brood_lag2)
+                                                                                                                                 PDO_brood_sync, PDO_brood_lag1, PDO_brood_lag2)
 
 # Correlation plot
 corr_Atnarko_sample_pdo <- round(cor(Atnarko_sample_pdo_correlation, use="pairwise.complete.obs"), 1)
@@ -111,8 +108,13 @@ chart.Correlation(Atnarko_sample_pdo_correlation , histogram=TRUE, pch=19)
 
 # Choose the PDO match that best correlates with the data (can pick up to 3 covariates)
 #rename the variable "Cov_" so forecastR will read it automatically as a covariate
-Atnarko_sample_age_forecastr<- Atnarko_sample_age_pdo_gathered %>% filter(PDO_match == "PDO_run_sync") %>% 
-                               rename(Cov_PDO = PDO.summer.av) %>% select(-c(Run_Year_Lag_1, Run_Year_Lag_2, Brood_Year_Lag_1, Brood_Year_Lag_2, PDO_match, Temperature_pattern))
+#Cov can't have any nas or will not work - so any "lag" variables need to be trimmed to the correct years
+Atnarko_sample_age_forecastr<- Atnarko_sample_age_pdo_gathered %>% 
+                               select(-c(Run_Year_Lag_1, Run_Year_Lag_2, Brood_Year_Lag_1, Brood_Year_Lag_2, Temperature_pattern)) %>% 
+                               filter(PDO_match %in% c("PDO_run_sync", "PDO_run_lag1")) %>% 
+                               pivot_wider(names_from=PDO_match, values_from=PDO.summer.av) %>% 
+                               rename(Cov_PDO_sync = PDO_run_sync, Cov_PDO_lag1 = PDO_run_lag1)
+
 
 #The Forecasting year needs to be the last year with data, keep as 2021 if there is 2021 data
 # Atnarko_sample_age_forecastr$Forecasting_Year[Atnarko_sample_age_forecastr$Forecasting_Year==2021]<-2018
