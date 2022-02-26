1106810bc5af99c03d51095e336de37b95e7e4c8
Brown <norah.brown2@gmail.com>
2022-02-25 17:39:06 -0800

    Mapping and matched lightstaion data to ERA_stocks

--- Norah_lightstations.R
+++ Norah_lightstations.R
@@ -19,6 +19,8 @@ Lightstations <-
 plot(Lightstations)
 
 
+View(Lightstations)
+
 nstations <- 12
 nmonths <- 12
 # 1914 is the year the first 'active' buoy went online
@@ -37,7 +39,7 @@ Data_Lightstations <- data.frame(Location=rep(NA,nstations*nyears*nmonths),
 # Loop through the currently active stations and obtain SST and salinity
 count_SST <- 1
 count_salinity <- 1
-for(filename in list.files('./DATA_Active_lightstations',pattern = '.csv') )
+for(filename in list.files('data-raw/DATA_Active_lightstations',pattern = '.csv') )
 {
   if(grepl(filename, pattern = 'Temperatures'))
   {
@@ -63,7 +65,7 @@ for(filename in list.files('./DATA_Active_lightstations',pattern = '.csv') )
         ignore.case = T)])
     
     # read the csv data and merge by Location Year Month
-    tmp <- read.csv(paste0('./DATA_Active_Lightstations/', filename), 
+    tmp <- read.csv(paste0('data-raw/DATA_Active_Lightstations/', filename), 
                     header = T, skip = 1)
     tmp <- tmp %>%
       dplyr::select(YEAR, JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC) %>%
@@ -111,7 +113,7 @@ for(filename in list.files('./DATA_Active_lightstations',pattern = '.csv') )
         ignore.case = T)])
     
     # read the data and merge by Location, Year and Month
-    tmp <- read.csv(paste0('./DATA_Active_Lightstations/', filename), 
+    tmp <- read.csv(paste0('data-raw/DATA_Active_Lightstations/', filename), 
                     header = T, skip = 1)
     tmp <- tmp %>%
       dplyr::select(YEAR, JAN, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC) %>%
@@ -148,13 +150,99 @@ Data_Lightstations <-
 # Check for missing values
 summary(Data_Lightstations)
 
-# Make it a spatial object in sf package with lat/lon CRS
-Data_Lightstations <-
-  sf::st_as_sf(Data_Lightstations,
-               coords=c('Longitude','Latitude'),
-               crs=sf::st_crs('EPSG:4326'))
+unique(Data_Lightstations$Location)
+#only 12 active lightstations
 
-# Convert to the same CRS as the data
-Data_Lightstations <-
-  sf::st_transform(Data_Lightstations,
-                   sf::st_crs(PACea::Coastline@proj4string))
+###
+Data_Lightstations_year_summer<- Data_Lightstations %>%  filter(Month %in% c(5,6,7,8,9)) %>% 
+                                 rename(year = Year) %>% 
+                                 group_by(Location, year) %>% 
+                                 dplyr::summarise(cov_SST_lighthouse_summer = mean(SST, na.rm=TRUE), 
+                                                  cov_PPT_lighthouse_summer = mean(Salinity, na.rm=TRUE))
+
+Data_Lightstations_year<- Data_Lightstations  %>% 
+                          rename(year = Year) %>%   
+                          group_by(Location, year) %>% 
+                          dplyr::summarise(cov_SST_lighthouse_year = mean(SST, na.rm=TRUE), 
+                                           cov_PPT_lighthouse_year = mean(Salinity, na.rm=TRUE))
+
+Data_Lightstations_combined<-merge(Data_Lightstations_year, Data_Lightstations_year_summer)
+Data_Lightstations_combined<- Data_Lightstations_combined %>% filter(year>1975) %>% as_tibble()
+Data_Lightstations_combined
+
+### Mapping
+
+Data_Lightstations_loc<-Data_Lightstations %>% select(Location, Longitude, Latitude) %>% 
+  rowwise() %>% distinct() %>% 
+  rename(long=Longitude, lat = Latitude, Site_name=Location)
+Data_Lightstations_loc$site_type<- "Lighthouse"
+
+
+light_stock<-bind_rows(Data_Lightstations_loc, stocks_loc_simple_BC)
+###Make map
+bbox_marine_light <- make_bbox(stocks_loc_simple_BC$long, stocks_loc_simple_BC$lat, f = 0.1)
+map_marine_light <- get_stamenmap(bbox_marine_light, source="stamen", maptype= "terrain", crop=FALSE, zoom=8)
+
+colorset_map_light = c("ERA Stock"="#FF007F" , "Lighthouse" ="#0000FF" )
+ggmap(map_marine_light) + geom_point(data=light_stock, aes(x = long, y = lat, col=site_type), size=3) +  scale_colour_manual(values=colorset_map_light)
+
+
+####Matching
+
+coordinates(Data_Lightstations_loc) <- c("long", "lat")
+coordinates(stocks_loc) <- c("long", "lat")             
+
+
+# Define these vectors, used in the loop.
+closestStock_ERAVec_light <- vector(mode = "numeric",length = nrow(stocks_loc))
+minDistVec_light     <- vector(mode = "numeric",length = nrow(stocks_loc))
+
+for (i in 1 : nrow(stocks_loc))
+{
+  distVec_light <- spDistsN1(Data_Lightstations_loc,stocks_loc[i,],longlat = TRUE)
+  minDistVec_light[i] <- min(distVec_light)
+  closestStock_ERAVec_light[i] <- which.min(distVec_light)
+}
+
+PointAssignTemps_light <- as(Data_Lightstations_loc[closestStock_ERAVec_light,]$Site_name,"character")
+
+loc_matching_light = data.frame(coordinates(stocks_loc),stocks_loc$Stock_ERA,stocks_loc$MapRegion,closestStock_ERAVec_light,minDistVec_light,PointAssignTemps_light)
+names(loc_matching_light) <- c("Stock_long","Stock_lat","Stock_ERA","Region","CloseTempIndex","Distance","Site_name")
+
+loc_matching_light<- loc_matching_light %>% arrange(desc(Distance)) %>% as_tibble()
+loc_matching_light
+
+loc_matching_light_BC<- loc_matching_light %>% filter(Region == "BC") %>% rename(Location = Site_name)
+loc_matching_light_BC
+
+loc_matching_light_BC_simple<-loc_matching_light_BC %>% select(Stock_ERA, Location)
+
+
+## Combing the match file to the data file
+Data_Lightstations_matched<-left_join(loc_matching_light_BC_simple, Data_Lightstations_combined)
+Data_Lightstations_matched<- Data_Lightstations_matched %>% rename(Lightstation = Location)
+Data_Lightstations_matched
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+# # Make it a spatial object in sf package with lat/lon CRS
+# Data_Lightstations <-
+#   sf::st_as_sf(Data_Lightstations,
+#                coords=c('Longitude','Latitude'),
+#                crs=sf::st_crs('EPSG:4326'))
+# 
+# # Convert to the same CRS as the data
+# Data_Lightstations <-
+#   sf::st_transform(Data_Lightstations,
+#                    sf::st_crs(PACea::Coastline@proj4string))
