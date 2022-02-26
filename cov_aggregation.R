19b338f0a9cf100bd8e28b1ece55061e2e902167
Brown <norah.brown2@gmail.com>
2022-02-25 17:40:41 -0800

    Merged data from lightstaions, buoys and pdo from noaa, graphed together on one plot per stock

--- cov_aggregation.R
+++ cov_aggregation.R
@@ -2,15 +2,42 @@
 
 
 
-#pdo
+#pdo #168
 pdo_simple
 
-#temperature from MEDS buoys
-dfo_meds_buoys_combined
+#temperature from MEDS buoys #464
+dfo_meds_buoys_matched_combined %>% as_tibble()
 
-
+#temp and salinity from lightstations #752
+Data_Lightstations_matched
 
 #combining PDO and temperature
 
-fcs_covariates<- left_join(dfo_meds_buoys_combined, pdo_simple) %>% as_tibble()
+fcs_covariates<- merge(Data_Lightstations_matched, dfo_meds_buoys_matched_combined, by=c("Stock_ERA", "year")) %>% as_tibble()
+fcs_covariates<-merge(fcs_covariates, pdo_simple, by=c("year")) %>% as_tibble()
 fcs_covariates
+
+
+
+
+
+fcs_covariates_long<- fcs_covariates %>% pivot_longer(cols = starts_with("cov"), names_to = "Covariate", values_to = "value") %>% 
+                                         mutate(var_cat = case_when(
+                                           str_detect(Covariate, "SST") ~ "Temperature", 
+                                           str_detect(Covariate, "PPT") ~ "Salinity",
+                                           str_detect(Covariate, "PDO") ~ "PDO"
+                                          )) %>% 
+                                        mutate(var_timing = case_when(
+                                          str_detect(Covariate, "year") ~ "Year", 
+                                          str_detect(Covariate, "summer") ~ "Summer"
+                                           )) %>% 
+                                         mutate(var_location = case_when(
+                                          str_detect(Covariate, "offshore") ~ "offshore", 
+                                          str_detect(Covariate, "terminal") ~ "terminal", 
+                                          TRUE ~ "terminal"
+                                         ))
+fcs_covariates_long
+
+ggplot(fcs_covariates_long %>% filter(Stock_ERA == "RBT", var_cat== "Temperature", var_location=="terminal"), 
+       aes(x=year, y=value, col=Covariate, group=Covariate))+
+        geom_point()+geom_line()+ggtitle("RBT")
