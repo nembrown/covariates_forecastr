c6acd8282ad4e1d9968149f758b0d0cd76bd9459
Brown <norah.brown2@gmail.com>
2022-03-07 13:05:59 -0800

    Added ONI and SOI

--- cov_aggregation.R
+++ cov_aggregation.R
@@ -4,13 +4,16 @@ library(ggplot2)
 
 # Check each of the datasets have been calculated properly ----------------
 
-#pdo #52 lines, all years sing 1896
+#NOAA pacific basin-wide variables: pdo, oni, soi
 pdo_simple
+oni_simple
+soi_simple
 
-#temperature from MEDS buoys #463, since 1989
+View(pdo_simple)
+#temperature from MEDS buoys #464, since 1989
 dfo_meds_buoys_matched_combined
 
-#temp and salinity from lightstations #752, since 1900
+#temp and salinity from lightstations #752, since 1900 but active lighthouses since 1976
 Data_Lightstations_matched
 
 #zooplankton from ios, since 1980
@@ -22,9 +25,12 @@ ios_zoop_anomalies<-ios_zoop_anomalies %>% rename(year = calc_year)
 
 fcs_covariates<- merge(Data_Lightstations_matched, dfo_meds_buoys_matched_combined, by=c("Stock_ERA", "year"), all=TRUE) %>% as_tibble()
 fcs_covariates<- merge(fcs_covariates,ios_zoop_anomalies, by=c("Stock_ERA", "year"), all=TRUE)
+fcs_covariates<- merge(fcs_covariates, soi_simple, by=c("year")) %>% as_tibble()
+fcs_covariates<- merge(fcs_covariates, oni_simple, by=c("year")) %>% as_tibble()
 fcs_covariates<- merge(fcs_covariates, pdo_simple, by=c("year")) %>% as_tibble()
 
-fcs_covariates<- fcs_covariates %>% relocate(where(is.numeric), .after = where(is.character))
+
+fcs_covariates<- fcs_covariates %>% relocate(where(is.numeric), .after = where(is.character)) %>% arrange(Stock_ERA, year)
 fcs_covariates
 
 write.csv(fcs_covariates, "fcs_covariates.csv")
