231568100b1a13f6e796ac777326f96e020daf74
Brown <norah.brown2@gmail.com>
2023-03-09 15:30:40 -0800

    added in truncate_ts functionality to include the california current data if desired

--- cov_correlate.R
+++ cov_correlate.R
@@ -10,32 +10,38 @@ library(rstatix)
 library(lares)
 library(purrr)
 
-
 # Read in cov_data for covariate and Av escapement  ------------------------------------------------------------
 
-correlate_covs(cov_data="fcs_covariates_interpolated.csv",
-               escapement_data = "Inputs/PPS_Esc_no_age_upto2022.csv",
-               modelstock = "PPS",
-               stock= "PPS",
+correlate_covs(cov_data_file="fcs_covariates_interpolated.csv",
+               escapement_data_file = "Inputs/MGS_Esc_upto2022.csv",
+               modelstock = "MGS",
+               stock= "BQR",
                year_match="Brood_Year_Lag1", 
                escapement_type="Average Escapement",
-               age_specific=FALSE, 
+               truncate_ts=1998,
+               age_specific=TRUE, 
                age_combine=FALSE,
-               age_class=NA)
+               age_class=5)
 
 
-correlate_covs<-function(cov_data,
-                         escapement_data,
+correlate_covs<-function(cov_data_file,
+                         escapement_data_file,
                          modelstock = NA_character_, 
                          stock= NA_character_, 
                          year_match= NA_character_,
                          escapement_type=NA_character_,
+                         truncate_ts=NA_integer_,
                          age_specific=FALSE,
                          age_combine=FALSE,
                          age_class = NA_integer_){
 
-  cov_data<-read.csv(cov_data) %>% as_tibble()
-  escapement_data<-read.csv(escapement_data) %>% as_tibble()
+  cov_data<-read.csv(cov_data_file) %>% as_tibble()
+  escapement_data<-read.csv(escapement_data_file) %>% as_tibble()
+  
+  if (!is.na(truncate_ts)){
+    escapement_data<-  escapement_data %>% filter(Brood_Year >= truncate_ts)
+  }
+  
   
   rel_file_dir <- normalizePath(paste0("Plots/", modelstock), mustWork = FALSE)
   
@@ -46,11 +52,16 @@ correlate_covs<-function(cov_data,
   
 cov_data_long<- cov_data %>% pivot_longer(cols = starts_with("cov"), names_to = "Covariate", values_to = "value") %>% 
   mutate(var_cat = case_when(
-    str_detect(Covariate, "SST") ~ "Temperature", 
-    str_detect(Covariate, "PPT") ~ "Salinity",
-    str_detect(Covariate, "zoop") ~ "Zooplankton", 
+    str_detect(Covariate, "SST|_T_|temp") ~ "Temperature", 
+    str_detect(Covariate, "PPT|salinity") ~ "Salinity",
+    str_detect(Covariate, "zoop|Ichthyoplankton") ~ "Zooplankton", 
+    str_detect(Covariate, "juvenile") ~ "Juvenile Salmon", 
+    str_detect(Covariate, "cope|Copepod") ~ "Copepod", 
+    str_detect(Covariate, "PC") ~ "Principal Components", 
     str_detect(Covariate, "herring") ~ "Herring",
     str_detect(Covariate, "water") ~ "Hydrographic", 
+    str_detect(Covariate, "Upwelling") ~ "Upwelling", 
+    str_detect(Covariate, "trans|Trans") ~ "Transition", 
     str_detect(Covariate, "ALPI") ~ "ALPI", 
     str_detect(Covariate, "PDO") ~ "PDO", 
     str_detect(Covariate, "SOI") ~ "SOI", 
@@ -90,11 +101,16 @@ cov_data_stock_roll<- zoo::rollmean(cov_data_stock %>% select(-Stock_ERA), k=3) 
 #Brood year
 cov_data_stock_roll_long<- cov_data_stock_roll %>% pivot_longer(cols = starts_with("cov"), names_to = "Covariate", values_to = "value") %>% 
   mutate(var_cat = case_when(
-    str_detect(Covariate, "SST") ~ "Temperature", 
-    str_detect(Covariate, "PPT") ~ "Salinity",
-    str_detect(Covariate, "zoop") ~ "Zooplankton", 
+    str_detect(Covariate, "SST|_T_|temp") ~ "Temperature", 
+    str_detect(Covariate, "PPT|salinity") ~ "Salinity",
+    str_detect(Covariate, "zoop|Ichthyoplankton") ~ "Zooplankton", 
+    str_detect(Covariate, "juvenile") ~ "Juvenile Salmon", 
+    str_detect(Covariate, "cope|Cope") ~ "Copepod", 
+    str_detect(Covariate, "PC") ~ "Principal Components", 
     str_detect(Covariate, "herring") ~ "Herring",
     str_detect(Covariate, "water") ~ "Hydrographic", 
+    str_detect(Covariate, "Upwelling") ~ "Upwelling", 
+    str_detect(Covariate, "trans|Trans") ~ "Transition", 
     str_detect(Covariate, "ALPI") ~ "ALPI", 
     str_detect(Covariate, "PDO") ~ "PDO", 
     str_detect(Covariate, "SOI") ~ "SOI", 
@@ -150,7 +166,7 @@ if(age_specific==TRUE & age_combine==FALSE){
   }
 
 
-### drop columns with NAs
+### drop columns with NAs 
 escapement_data_covariates_wide<-  escapement_data_covariates_wide %>% select(Escapement_type, contains("Brood"), contains("Run"), (contains("cov") & where(~! any(is.na(.)))))
 escapement_data_covariates<-  escapement_data_covariates %>% group_by(Covariate) %>% filter(!is.na(sum(value))) %>% ungroup()
 
@@ -183,17 +199,69 @@ for(Covariate_ in matches_stock) {
 }
 
 ########### Covariate_plots_stock
+#California_current_ocean indices
+california_current_oceanic_Covariate_plots_stock <- pluck(Covariate_plots_stock, "cov_PDO_Sum_Dec_March", .default=plot_spacer()) + 
+  pluck(Covariate_plots_stock, "cov_PDO_Sum_May_Sept", .default=plot_spacer())+
+  pluck(Covariate_plots_stock, "cov_ONI_Average_Jan_June", .default=plot_spacer()) + 
+  pluck(Covariate_plots_stock, "cov_Physical_Spring_Trans", .default=plot_spacer()) +
+  pluck(Covariate_plots_stock, "cov_Physical_Spring_Trans_Hydro", .default=plot_spacer()) +
+  pluck(Covariate_plots_stock, "cov_Upwelling_Anomaly_Sum_April_May", .default=plot_spacer()) +
+  pluck(Covariate_plots_stock, "cov_Length_Upwelling_season", .default=plot_spacer()) +
+  pluck(Covariate_plots_stock, "cov_PC1", .default=plot_spacer()) +
+  pluck(Covariate_plots_stock, "cov_PC2", .default=plot_spacer()) +
+    guide_area()+
+  plot_layout(guides = 'collect', ncol=3)
+
+if (!is.na(truncate_ts) & truncate_ts >=1998) {
+ggsave(california_current_oceanic_Covariate_plots_stock, file=paste0("Plots/", modelstock, "/cali_current_oceanic_", year_match,"_", modelstock, "_Age_",age_class, ".tiff"))
+}
+
+#cali current temperature/salinity
+california_current_temp_salinity_Covariate_plots_stock <- pluck(Covariate_plots_stock, "cov_SST_NDBC_buoys_May_Sept", .default=plot_spacer()) + 
+  pluck(Covariate_plots_stock, "cov_Upper_20_m_T_Nov_Mar", .default=plot_spacer())+
+  pluck(Covariate_plots_stock, "cov_Upper_20_m_T_May_Sept", .default=plot_spacer()) + 
+  pluck(Covariate_plots_stock, "cov_Deep_temperature_May_Sept", .default=plot_spacer()) +
+  pluck(Covariate_plots_stock, "cov_Deep_salinity_May_Sept", .default=plot_spacer()) +
+  guide_area()+
+  plot_layout(guides = 'collect', ncol=3)
+
+if (!is.na(truncate_ts) & truncate_ts >=1998) {
+  ggsave(california_current_temp_salinity_Covariate_plots_stock, file=paste0("Plots/", modelstock, "/cali_current_temp_sal_", year_match,"_", modelstock, "_Age_",age_class, ".tiff"))
+}
+
+#cali current zooplankton
+california_current_zoop_Covariate_plots_stock <- pluck(Covariate_plots_stock, "cov_Copepod_richness_anom_May_Sept", .default=plot_spacer()) + 
+  pluck(Covariate_plots_stock, "cov_N_copepod_biomass_anom_May_Sept", .default=plot_spacer())+
+  pluck(Covariate_plots_stock, "cov_S_copepod_biomass_anom_May_Sept", .default=plot_spacer()) + 
+  pluck(Covariate_plots_stock, "cov_Nearshore_Ichthyoplankton_Jan_Mar", .default=plot_spacer()) +
+  pluck(Covariate_plots_stock, "cov_Near_off_community_Ichthyoplankton_Jan_Mar", .default=plot_spacer()) +
+  pluck(Covariate_plots_stock, "cov_Copepod_Community_Index_May_Sept", .default=plot_spacer()) +
+  
+    guide_area()+
+  plot_layout(guides = 'collect', ncol=3)
+
+if (!is.na(truncate_ts) & truncate_ts >=1998) {
+  ggsave(california_current_zoop_Covariate_plots_stock, file=paste0("Plots/", modelstock, "/cali_current_zoop", year_match,"_", modelstock, "_Age_",age_class, ".tiff"))
+}
+
+#cali current juvenile salmon
+california_current_salmon_Covariate_plots_stock <- pluck(Covariate_plots_stock, "cov_Chinook_salmon_juvenile_catches_June", .default=plot_spacer()) + 
+  pluck(Covariate_plots_stock, "cov_Coho_salmon_juvenile_catches_June", .default=plot_spacer())+
+  guide_area()+
+  plot_layout(guides = 'collect', ncol=2)
+
+if (!is.na(truncate_ts) & truncate_ts >=1998) {
+  ggsave(california_current_salmon_Covariate_plots_stock, file=paste0("Plots/", modelstock, "/cali_current_salmon", year_match,"_", modelstock, "_Age_",age_class, ".tiff"))
+}
+
+
 #Oceanic indices by year
 oceanic_yearly_Covariate_plots_stock <- pluck(Covariate_plots_stock, "cov_PDO_yearly_mean", .default=plot_spacer()) + 
   pluck(Covariate_plots_stock, "cov_ONI_yearly_mean", .default=plot_spacer())+
   pluck(Covariate_plots_stock, "cov_ONI_yearly_anomaly", .default=plot_spacer()) + 
   pluck(Covariate_plots_stock, "cov_SOI_yearly_mean", .default=plot_spacer()) +
-  # pluck(Covariate_plots_stock, "cov_NPI_yearly_mean", .default=plot_spacer()) +
-  # pluck(Covariate_plots_stock, "cov_NPI_yearly_anomaly", .default=plot_spacer()) +
   pluck(Covariate_plots_stock, "cov_NPGO_yearly_mean", .default=plot_spacer()) +
   pluck(Covariate_plots_stock, "cov_EPNP_yearly_mean", .default=plot_spacer()) +
- # pluck(Covariate_plots_stock, "cov_ALPI_yearly_mean", .default=plot_spacer()) +
- # pluck(Covariate_plots_stock, "cov_model_EVs", .default=plot_spacer()) +
   guide_area()+
   plot_layout(guides = 'collect', ncol=3)
 
@@ -219,7 +287,14 @@ zoop_Covariate_plots_stock <-  pluck(Covariate_plots_stock, "cov_zoop_yearly_ano
   pluck(Covariate_plots_stock, "cov_zoop_spring_anomaly", .default=plot_spacer()) + 
   pluck(Covariate_plots_stock, "cov_zoop_fall_anomaly", .default=plot_spacer()) + 
   plot_layout(guides = 'collect', ncol=3)
+
+if (pluck_exists(Covariate_plots_stock, "cov_zoop_yearly_anomaly") == TRUE | 
+    pluck_exists(Covariate_plots_stock, "cov_zoop_winter_anomaly") == TRUE |
+    pluck_exists(Covariate_plots_stock, "cov_zoop_summer_anomaly")== TRUE |
+    pluck_exists(Covariate_plots_stock, "cov_zoop_spring_anomaly") == TRUE |
+    pluck_exists(Covariate_plots_stock, "cov_zoop_fall_anomaly")== TRUE) {
 ggsave(zoop_Covariate_plots_stock,  file=paste0("Plots/", modelstock, "/zoop_",  year_match,"_", modelstock, "_Age_",age_class, ".tiff"))
+}
 
 #Temperature
 temp_Covariate_plots_stock <- 
@@ -230,21 +305,32 @@ temp_Covariate_plots_stock <-
   pluck(Covariate_plots_stock, "cov_SST_lighthouse_yearly_mean", .default=plot_spacer()) + 
   pluck(Covariate_plots_stock, "cov_SST_lighthouse_summer_mean", .default=plot_spacer()) + 
   plot_layout(guides = 'collect', ncol=2)
+if (pluck_exists(Covariate_plots_stock, "cov_SST_lighthouse_yearly_mean")==TRUE |
+    pluck_exists(Covariate_plots_stock, "cov_SST_lighthouse_summer_mean")==TRUE ){
 ggsave(temp_Covariate_plots_stock, file=paste0("Plots/", modelstock, "/temperature_",  year_match,"_", modelstock, "_Age_",age_class, ".tiff"))
+}
 
 #Salinity
 salinity_Covariate_plots_stock <-  pluck(Covariate_plots_stock, "cov_PPT_lighthouse_yearly_mean", .default=plot_spacer()) + 
   pluck(Covariate_plots_stock, "cov_PPT_lighthouse_summer_mean", .default=plot_spacer()) + 
   plot_layout(guides = 'collect', ncol=2)
+if (pluck_exists(Covariate_plots_stock, "cov_PPT_lighthouse_yearly_mean")==TRUE |
+    pluck_exists(Covariate_plots_stock, "cov_PPT_lighthouse_summer_mean")==TRUE ){
 ggsave(salinity_Covariate_plots_stock, file=paste0("Plots/", modelstock, "/salinity_",  year_match,"_", modelstock, "_Age_",age_class, ".tiff"))
+}
 
 #Herring
 herring_Covariate_plots_stock <-  pluck(Covariate_plots_stock, "cov_herring_spawn_index_mean", .default=plot_spacer())
+if (pluck_exists(Covariate_plots_stock, "cov_herring_spawn_index_mean")==TRUE){
 ggsave(herring_Covariate_plots_stock,  file=paste0("Plots/", modelstock, "/herring_", year_match,"_", modelstock,  "_Age_",age_class,".tiff"))
+}
 
 #Hydrographic variables
 hydro_Covariate_plots_stock <-  pluck(Covariate_plots_stock, "cov_water_flow_yearly_mean", .default=plot_spacer())+
   pluck(Covariate_plots_stock, "cov_water_flow_yearly_max", .default=plot_spacer())+ 
   plot_layout(guides = 'collect', ncol=2)
+if (pluck_exists(Covariate_plots_stock, "cov_water_flow_yearly_mean")==TRUE |
+    pluck_exists(Covariate_plots_stock, "cov_water_flow_yearly_max")==TRUE ){
 ggsave(hydro_Covariate_plots_stock,  file=paste0("Plots/", modelstock, "/hydro_",  year_match,"_", modelstock, "_Age_",age_class, ".tiff"))
+}
 }
